b56e8c57eaff97c0358bbffd697b418d5eae63cb
Jireh <jireh_tan@mymail.sutd.edu.sg>
2019-10-14 12:15:01 +0800

    Revert "updated keys.js"
    This reverts commit 83bdfcf664aeeae2970afb73a3e89aaa2bb2635a.

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mongoURI: "mongodb://phangster:1234@education-shard-00-00-x2lxj.mongodb.net:27017,education-shard-00-01-x2lxj.mongodb.net:27017,education-shard-00-02-x2lxj.mongodb.net:27017/edu_hack?ssl=true&replicaSet=education-shard-0&authSource=admin&retryWrites=true&w=majority",
-  mongoURI:"mongodb://myUser2:password@3.214.81.197/test",
+  mongoURI: "mongodb://phangster:1234@education-shard-00-00-x2lxj.mongodb.net:27017,education-shard-00-01-x2lxj.mongodb.net:27017,education-shard-00-02-x2lxj.mongodb.net:27017/edu_hack?ssl=true&replicaSet=education-shard-0&authSource=admin&retryWrites=true&w=majority",
+  // mongoURI:"mongodb://myAdmin:password@3.214.81.197/test",
   secretOrKey: "secret",
   mySQLParams: {host: "localhost", user: "root", password:"password", database:"test"}
 };

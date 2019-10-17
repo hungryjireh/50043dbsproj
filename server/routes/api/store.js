2a290f321dcbb06d69d2bb58869dbdc07f108658
Jireh <jireh_tan@mymail.sutd.edu.sg>
2019-10-17 17:46:48 +0800

    configured SQL and Mongo API endpoints

--- server/routes/api/store.js
+++ server/routes/api/store.js
@@ -9,6 +9,7 @@ const Logs = require("../../models/Logs");
 // @get all books in store
 // @access Public
 
+// API Endpoint: http://localhost:5000/api/store/getbooks
 router.get('/getbooks', (req, res, next) => {
     Store.find((err, data) => {
         if (err) {
@@ -37,8 +38,10 @@ router.get('/getbooks', (req, res, next) => {
     }).limit(50);
 });
 
+// API Endpoint: http://localhost:5000/api/store/book/:bookid
 router.get('/book/:bookid', (req, res, next) => {
-    Store.findOne({ asin:  req.params.bookid }, (err, data) => {
+    const asin = req.params.bookid;
+    Store.findOne({ 'asin':  asin }, (err, data) => {
         if (err) {
             const newLogs = new Logs({
                 timestamp: Date.now(),
@@ -104,6 +107,55 @@ router.get('/search', (req, res, next) => {
         }
     })
     .limit(50);
+});
+
+// API Endpoint: http://localhost:5000/api/store/:bookid
+// NOTE: THIS SAVES THE BOOK TO THE MAIN MONGODB BOOK COLLECTION.
+router.post('/:bookID', (req, res, next) => {
+
+    const bookID = req.params.bookID;
+    const bookData = req.body;
+
+    Store.findOne({ bookID }).then(store => {
+        // Check if book exists
+        if (store) {
+          const newLogs = new Logs({
+              timestamp: Date.now(),
+              database: "Mongo - Books",
+              method: "POST",
+              userID: null,
+              parameters: "/:bookID - " + bookID + ". Book already exists",
+              response: "404"
+          });
+          newLogs.save();
+          return res.status(404).json({ booknotfound: "Book already exists" });
+        } else {
+            const newBook = new Store({
+                "asin": bookID,
+                "title": bookData.title,
+                "description": bookData.description,
+                "price": bookData.price,
+                "imUrl": bookData.imUrl,
+                "related": bookData.related,
+                "salesRank" : bookData.salesRank,
+                "brand" : bookData.brand,
+                "categories": bookData.categories
+            });
+            newBook.save()
+            const newLogs = new Logs({
+                timestamp: Date.now(),
+                database: "Mongo - Books",
+                method: "POST",
+                userID: null,
+                parameters: "/:bookID - " + bookID,
+                response: "200"
+            });
+            newLogs.save();
+            return res
+              .status(200)
+              .json({ bookData: "success" });
+        }
+    });
 });
 
 module.exports = router;

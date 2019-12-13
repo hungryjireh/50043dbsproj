972a298fe77bbab3f5c22afdcd5cfb9c1cb1d72c
Jireh <hungryjireh@gmail.com>
2019-12-13 10:25:52 +0800

    updated folders

--- deployment_share/backend/mongo_commands.js
+++
@@ -1,7 +0,0 @@
-use admin
-db.createUser({user: "admin", pwd: "password", roles: [{ role: "userAdminAnyDatabase", db: "admin" }, { role: "readWriteAnyDatabase", db: "admin" }, { role: "dbAdminAnyDatabase",   db: "admin" }]});
-db.createUser({user: "myUser2", pwd: "password", roles: [{ role: "readWrite", db: "test" }, { role: "readWrite", db: "logs" }, { role: "dbAdmin",   db: "test" },  { role: "dbAdmin",   db: "logs" }]});
-use logs
-db.logs.insert({1: "hello!"})
-use test
-db.users.insert({1: "hello!"})

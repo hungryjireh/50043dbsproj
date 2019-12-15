acc849996e9c9030cf3dfcaba5362cf3a0855f7f
Jakub Lecbych <jakub.lecbych@avast.com>
2019-12-15 14:08:58 +0800

    fix - dashboard not loading books

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -53,12 +53,15 @@ class CardDesign extends React.Component {
       posts: {}
     }
   }
-  componentWillMount() {
-    this.setState({
-      posts: PostsData
-    });
-  }
- 
+  componentDidMount() {
+      this.getDataFromDb();
+  };
+
+  getDataFromDb = () => {
+      fetch("http://localhost:5000/api/store/getbooks")
+          .then(data => data.json())
+          .then(res => this.setState({ posts: res.data }));
+  };
 
   render() {
     return <div>
@@ -88,50 +91,63 @@ class Title extends React.Component {
 
 
 class Button extends React.Component {
-  render() {
-    return (
-      // <Link to={`/project/${user.id}`}>
-      <Link to={'/book/'}>
-        <button className="button button-primary button-improve">
-          <i className="fa fa-chevron-right"></i> Find out more
-        </button>
-      </Link>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        const id = event.target.value;
+        console.log(id);
+    }
+
+    render() {
+        const { asin, index } = this.props;
+        return (
+            <div>
+                <Link to={`/book/${asin}`}>
+                    <button onClick={this.handleClick} value={asin} className="button button-primary marketplace-button-improve">
+                        <i className="fa fa-chevron-right"></i> Find out more
+                    </button>
+                </Link>
+            </div>
+        )
+    }
 }
 
 
 class CardHeader extends React.Component {
-  render() {
-    const { image, category } = this.props;
-    var style = { 
-        backgroundImage: 'url(' + image + ')',
-        height: '15rem',
-        textAlign: 'center'
-    };
-    return (
-      <header style={style} className="card-header">
-        <h4 className="card-header--title">{category}</h4>
-      </header>
-    )
-  }
+    render() {
+        const { imUrl, asin } = this.props;
+        var style = {
+            backgroundImage: 'url(' + imUrl + ')',
+            height: '15rem',
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+        };
+        return (
+            <header style={style} className="card-header">
+                <h4 className="card-header--title">{asin}</h4>
+            </header>
+        )
+    }
 }
 
 
 class CardBody extends React.Component {
-  render() {
-    return (
-      <div className="card-body">
-        <p className="date">March 20 2015</p>
-        
-        <h2>{this.props.title}</h2>
-        
-        <p className="body-content">{this.props.text}</p>
-        
-        <Button />
-      </div>
-    )
-  }
+    render() {
+        const { description, price, asin, index} = this.props;
+        return (
+            <div className="card-body">
+                <h2 className="limit-box">{description}</h2>
+                <div>
+                    <p className="body-content">{price}</p>
+                </div>
+                <Button asin={asin} index={index}/>
+            </div>
+        )
+    }
 }
 
 
@@ -139,8 +155,8 @@ class Card extends React.Component {
   render() {
     return (
       <article className="card">
-        <CardHeader category={this.props.details.category} image={this.props.details.image}/>
-        <CardBody title={this.props.details.title} text={this.props.details.text}/>
+          <CardHeader asin={this.props.details.asin} imUrl={this.props.details.imUrl}/>
+          <CardBody index={this.props.index} asin={this.props.details.asin} description={this.props.details.description} price={this.props.details.price}/>
       </article>
     )
   }
